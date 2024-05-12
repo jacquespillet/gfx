adf666d91a950694d27b905a0def65eefa75b959
jacquespillet <jacques@scanlabprojects.co.uk>
2024-05-12 11:35:55 +0200

    removed ext

--- ext/glslang/lib/cmake/HLSLTargets.cmake
+++
@@ -1,9 +0,0 @@
-
-        message(WARNING "Using `HLSLTargets.cmake` is deprecated: use `find_package(glslang)` to find glslang CMake targets.")
-
-        if (NOT TARGET glslang::HLSL)
-            include("C:/Users/jacqu/Documents/Boulot/Libs/glslang-main/build/install/lib/cmake/glslang/glslang-targets.cmake")
-        endif()
-
-        add_library(HLSL ALIAS glslang::HLSL)
-    
